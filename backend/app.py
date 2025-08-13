b8f367e23278e65f0e012be405cd1b2e261ae367
Shreesha <shreeshhegde007@gmail.com>
2025-08-13 18:26:10 +0530

    changed if = main port compatible to run on render

--- backend/app.py
+++ backend/app.py
@@ -320,4 +320,6 @@ def page_not_found(_):
 
 # ── Run ────────────────────────────────────────────────────
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
